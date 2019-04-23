0f29aabd1f9f3794c08a2101a716c9ce94054eef
guodonglw <44934555+guodonglw@users.noreply.github.com>
2019-04-23 17:12:12 +0800

    Update 我的表结构.sql

--- server/documents/我的表结构.sql
+++ server/documents/我的表结构.sql
@@ -14,12 +14,12 @@ CREATE TABLE `user` (
 # 账目表单
 CREATE TABLE user_account
 (
-	AccountID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,        # 账目ID
-	ID INT NOT NULL,                                          # 管理员ID
+  AccountID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,        # 账目ID
+  ID INT NOT NULL,                                          # 管理员ID
   RealName VARCHAR(20) NOT NULL,                            # 管理员名字
-	InOrOut INT DEFAULT 1 NOT NULL,                           # 1、收入 2、支出
-	Amount INT DEFAULT 0 NOT NULL,                            # 实际金额
-	RecoudTime TIMESTAMP DEFAULT now() NOT NULL               # 记录时间
+  InOrOut INT DEFAULT 1 NOT NULL,                           # 1、收入 2、支出
+  Amount INT DEFAULT 0 NOT NULL,                            # 实际金额
+  RecoudTime TIMESTAMP DEFAULT now() NOT NULL               # 记录时间
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE user_account ADD
